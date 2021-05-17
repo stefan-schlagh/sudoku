83b88936e41a78c57639c0cd53733716ba7460e4
stefan-schlagh <64643082+stefan-schlagh@users.noreply.github.com>
2021-05-17 15:49:26 +0200

    generate numbers

--- index.js
+++ index.js
@@ -5,31 +5,61 @@ const area = document.getElementById("area")
 const grid = new Array(9)
 const gridElements = new Array(9)
 
-for(row of grid){
-    row = new Array(9)
-    for(col of row){
-        col = 0
-        //area.appendChild(document.createElement("div"))
+let generatedNumbers
+
+// create the grid
+function createGrid(){
+
+    for(let row of grid){
+        row = new Array(9)
+        for(let col of row){
+            col = 0
+            //area.appendChild(document.createElement("div"))
+        }
+    }
+    let tBody;
+    for(let i=0;i<9;i++){
+        if(i%3 == 0){
+            tBody = document.createElement("tbody")
+            area.appendChild(tBody)
+        }
+        gridElements[i] = new Array(9)
+
+        const row = document.createElement("tr")
+        tBody.appendChild(row)
+
+        for(let j=0;j<9;j++){
+            const colElement = document.createElement("td")
+            gridElements[i][j] = colElement
+            //colElement.classList.add("grid-item")
+            colElement.appendChild(document.createTextNode(""))
+            tBody.appendChild(colElement)
+        }
+    }
+    // add event listener for generate
+    document.getElementById("generate").addEventListener("click",() => {
+        fillGridWithGeneratedNumbers()
+    })
+}
+
+function fillGridWithGeneratedNumbers(){
+    // get difficulty
+    const difficulty = document.getElementById("difficulty").value
+    // generate numbers
+    generatedNumbers = sudoku.generate(difficulty)
+    // fill grid with numbers
+    for(let i=0;i<9;i++){
+        for(let j=0;j<9;j++){
+            const gridElement = gridElements[i][j]
+            // get char
+            let char = generatedNumbers.charAt(i*9 + j)
+            // if char is . , use ␣ 
+            if(char == ".")
+                char = " "
+            gridElement.innerText = char
+        }
     }
 }
-let tBody;
-for(let i=0;i<9;i++){
-    if(i%3 == 0){
-        tBody = document.createElement("tbody")
-        area.appendChild(tBody)
-    }
-    gridElements[i] = new Array(9)
-    rowElements = gridElements[i]
 
-    const row = document.createElement("tr")
-    tBody.appendChild(row)
-
-    for(colElement of rowElements){
-        colElement = document.createElement("td")
-        //colElement.classList.add("grid-item")
-        colElement.appendChild(document.createTextNode(""))
-        tBody.appendChild(colElement)
-    }
-}
-
-console.log(sudoku.generate(60))
+createGrid()
+fillGridWithGeneratedNumbers()
